19bcf60528069ce9651b080a5e79266fa1811c5e
Mir0069 <malhaarlalitmirchandani@gmail.com>
2024-08-30 23:13:37 +0530

    update:home banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import logo from './images/logo.png';
+import Working from './images/working.svg';
 
 const Home = () => {
   return (
-    <div className='top-20 relative block pb-20 justify-around'>
-    <div className='flex'>
-      <div className='w-1/2 pl-4'>hello</div>
-      <div className='w-1/2'><img src={logo} className='w-11' /></div>
-    </div>
-    
-    
-    
+    <div className='top-20 relative block pb-20 justify-around h-[80vh]'>
+      <div className='flex'>
+        <div className='w-1/2  mt-32 ml-44 font-extrabold text-5xl'> 
+        Are you looking for Freelancers?
+        <p className='text-xl font-light pt-6'>Hire Great Freelancers, Fast. Spacelance helps you hire elite freelancers at a moment's notice</p>
+        </div>
+        
+        <div className=' mt-12 w-1/2 '><img src={Working} className=' ml-32 w-1/2' /></div>
+      </div>
+
+
+
     </div>
 
   )

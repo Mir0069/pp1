77ac8f738de9bf692108bef759dbc05d6f9b4c61
Mir0069 <malhaarlalitmirchandani@gmail.com>
2024-08-31 15:19:27 +0530

    Fix:cards content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -117,19 +117,19 @@ const Home = () => {
                     d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
 
-                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h2>
+                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Required React Developer</h2>
               </div>
 
               <div
                 className="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 group-hover:w-72 sm:p-6 lg:p-8">
-                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h3>
+                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Required React Developer</h3>
 
                 <p className="mt-4 text-sm sm:text-base">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem
                   omnis atque culpa repellendus.
                 </p>
 
-                <p className="mt-8 font-bold">Read more</p>
+                
               </div>
             </div>
           </span>
@@ -145,19 +145,19 @@ const Home = () => {
                     d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
 
-                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h2>
+                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Required Graphic Designer</h2>
               </div>
 
               <div
                 className="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 group-hover:w-72 sm:p-6 lg:p-8">
-                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h3>
+                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Required Graphic Designer</h3>
 
                 <p className="mt-4 text-sm sm:text-base">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem
                   omnis atque culpa repellendus.
                 </p>
 
-                <p className="mt-8 font-bold">Read more</p>
+              
               </div>
             </div>
           </span>
@@ -173,19 +173,19 @@ const Home = () => {
                     d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
 
-                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h2>
+                <h2 className="mt-4 text-xl font-medium sm:text-2xl">Requuired Illustrator expert</h2>
               </div>
 
               <div
                 className="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:w-72 group-hover:opacity-100 sm:p-6 lg:p-8">
-                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Go around the world</h3>
+                <h3 className="mt-4 text-xl font-medium sm:text-2xl">Requuired Illustrator expert</h3>
 
                 <p className="mt-4 text-sm sm:text-base">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem
                   omnis atque culpa repellendus.
                 </p>
 
-                <p className="mt-8 font-bold">Read more</p>
+               
               </div>
             </div>
           </span>
